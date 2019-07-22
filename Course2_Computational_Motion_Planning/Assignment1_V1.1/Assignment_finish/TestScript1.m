794a2f2514dc1ccaddfd3511d39bf35fdf25b5df
Airu <b10307115@gapps.ntust.edu.tw>
2019-07-23 00:15:28 +0800

    update

---
+++ Course2_Computational_Motion_Planning/Assignment1_V1.1/Assignment_finish/TestScript1.m
@@ -0,0 +1,19 @@
+%
+% TestScript for Assignment 1
+%
+
+%% Define a small map
+map = false(10);
+
+% Add an obstacle
+map (1:5, 6) = true; %Obstacle Declaration
+start_coords = [6, 2]; %Starting Coordinates
+dest_coords  = [8, 9]; %Destination Coordinates
+
+%%
+close all;
+% [route, numExpanded] = DijkstraGrid (map, start_coords, dest_coords);
+% Uncomment following line to run Astar
+[route, numExpanded] = AStarGrid (map, start_coords, dest_coords);
+
+%HINT: With default start and destination coordinates defined above, numExpanded for Dijkstras should be 76, numExpanded for Astar should be 23.
